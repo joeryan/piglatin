5d4eea642abb41ab3fe06210c7096408a494f03d
Joe Ryan <joe@clanryan.us>
2024-11-05 20:53:31 -0500

    completed pig latin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 fn main() {
     // TODO if wanted get string from user as a cli
-    let old_string = "This donkey went to town and came home.";
+    let old_string = "This donkey went to town and came home";
     let mut output_words: Vec<String> = Vec::new();
     // for each word in string:
     for word in old_string.split(' ') {
@@ -21,20 +21,16 @@ fn make_pig_word(word: &str) -> String {
     let vowels = "aAeEiIoOuU";
     let mut pig_word = String::from(word);
     let first_char = pig_word.remove(0);
-    match first_char {
-        Some(first_char) => {
-            if vowels.contains(first_char) {
-                let mut pig_word = String::from(word);
-                pig_word.push_str("-hay");
-            } else {
-                let mut pig_suffix = String::from("-");
-                pig_suffix.push(first_char);
-                pig_suffix.push_str("ay");
-                println!("suffix: {pig_suffix}");
-                pig_word.push_str(&pig_suffix);
-            }
-        }
-        _ => (),
+    if vowels.contains(first_char) {
+        let mut tmp_word = String::from(word);
+        tmp_word.push_str("-hay");
+        pig_word = tmp_word;
+    } else {
+        let mut pig_suffix = String::from("-");
+        pig_suffix.push(first_char);
+        pig_suffix.push_str("ay");
+        println!("suffix: {pig_suffix}");
+        pig_word.push_str(&pig_suffix);
     }
     return pig_word;
 }
